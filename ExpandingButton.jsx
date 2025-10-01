cdff295b5a1942bc83859eef354c495496359790
saegfa <okaninal15@gmail.com>
2025-10-01 17:49:57 +0300

    There will be package.json for future elements

---
+++ ExpandingButton.jsx
@@ -0,0 +1,12 @@
+export default function ExpandingButton(props) {
+  return (
+    <div
+      onClick={/*onClick*/}
+      style={/**/}
+      key={/*key*/}
+      className={`flex-1 ${/*color*/} flex items-center justify-center text-white text-xl font-bold cursor-pointer transition-all duration-400 ease-in-out hover:flex-[8]`}
+    >
+      {/*label*/}
+    </div>
+  )
+}
