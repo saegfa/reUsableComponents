d68d444d6e7849164f5a953855b32918d8205d51
saegfa <okaninal15@gmail.com>
2025-10-03 15:16:18 +0300

    read the comment lines before using it

--- ExpandingButton.jsx
+++ ExpandingButton.jsx
@@ -10,3 +10,38 @@ export default function ExpandingButton(props) {
     </div>
   )
 }
+//necessary dependecies on package.json
+/*"autoprefixer": "^10.4.21",
+    "postcss": "^8.5.6",
+    "tailwindcss": "^4.1.13",*/
+
+//index.css
+/*@tailwind base;
+@tailwind components;
+@tailwind utilities;*/
+
+//src files
+/*postcss.config.js
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+ */
+//tailwind.config.js
+// /** @type {import('tailwindcss').Config} */
+//export default {
+//  content: ["./index.html", "./src/**/*.{js,jsx}"],
+//  theme: {
+//    extend: {},
+//  },
+//  plugins: [],
+//};
+
+/* to call
+      <div className="flex w-[900px] h-[600px] overflow-hidden rounded-xl shadow-lg mt-16">
+        {buttons.map((btn) => (
+          <ExpandingButton btn={btn}/>
+        ))}
+      </div>
+ */
